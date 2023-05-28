8b9e507ca57912c350e8d6252fa8c0f5ab819f20
elizantonius <lonecode1777@gmail.com>
2023-05-28 08:09:15 +0700

    Komit Pertama

---
+++ Models/Berita.cs
@@ -0,0 +1,12 @@
+﻿namespace bzbackend.Models
+{
+    public class Berita
+    {
+        public int id { get; set; }
+        public String judul { get; set; }
+        public String gambar { get; set; }
+        public String isi { get; set; }
+        public DateTime tanggal { get; set; }
+
+    }
+}
