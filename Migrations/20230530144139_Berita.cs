16183a8c0e8eab055b514dae8ad17ba79c91479a
elizantonius <lonecode1777@gmail.com>
2023-05-30 21:45:06 +0700

    Fix Migrations

---
+++ Migrations/20230530144139_Berita.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bzbackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Berita : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
