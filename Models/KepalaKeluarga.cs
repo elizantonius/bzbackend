f347f9678d0be8010d06320683e13adc588ec614
elizantonius <lonecode1777@gmail.com>
2023-12-03 03:40:06 +0700

    Add Model

---
+++ Models/KepalaKeluarga.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bzbackend.Models
+{
+    public class KepalaKeluarga
+    {
+        [Key]
+
+        public int id { get; set; }
+    }
+}
