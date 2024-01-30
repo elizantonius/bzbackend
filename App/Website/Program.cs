9511446ffe804d0f5ff0d9cc2c89a30d5da88e61
elizantonius <lonecode1777@gmail.com>
2024-01-31 03:43:39 +0900

    Add Blazor Bootstrap

--- App/Website/Program.cs
+++ App/Website/Program.cs
@@ -2,6 +2,8 @@ using Website.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddBlazorBootstrap();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
