ac035bfde8e65ed48dd7b36c3cf2708425e1f1d0
elizantonius <lonecode1777@gmail.com>
2024-06-22 16:37:36 +0900

    tests

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
     throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
